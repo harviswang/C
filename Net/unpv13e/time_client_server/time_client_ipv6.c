2417e8acd4ab4bcd6df4627c65ec861ce8c28d18
Harvis Wang <jiankangshiye@aliyun.com>
2017-06-04 15:44:14 +0000

    add time_client_server

---
+++ Net/unpv13e/time_client_server/time_client_ipv6.c
@@ -0,0 +1,56 @@
+#include <stdio.h> /* strerror fprintf */
+#include <unistd.h> /* read */
+#include <string.h> /* memset */
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MAXLINE 32
+
+int main(int argc, char *argv[])
+{
+	int sock_fd, n;
+	char recv_line[MAXLINE + 1];
+	struct sockaddr_in6 serv_addr;
+
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s <IPaddress>\n", argv[0]);
+		return -1;
+	}
+
+	if ( (sock_fd = socket(AF_INET6, SOCK_STREAM, 0)) < 0) {
+		fprintf(stderr, "socket error:%s\n", strerror(errno));
+		return -1;
+	}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin6_family = AF_INET6;
+	serv_addr.sin6_port = htons(13);
+	if (inet_pton(AF_INET6, argv[1], &serv_addr.sin6_addr) <= 0) {
+		fprintf(stderr, "inet_pton error:%s\n", strerror(errno));
+		return -1;
+	}
+
+	if (connect(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		fprintf(stderr, "connect error:%s\n", strerror(errno));
+		return -1;
+	}
+
+	while ( (n = read(sock_fd, recv_line, MAXLINE)) > 0) {
+		recv_line[n] = 0;
+		if (fputs(recv_line, stdout) == EOF) {
+			fprintf(stderr, "fputs error:%s\n", strerror(errno));
+			return -1;
+		}
+	}
+
+	if (n < 0) {
+		fprintf(stderr, "read error:%s\n", strerror(errno));
+		return -1;
+	}
+
+	return 0;
+}
+
---
+++ Net/unpv13e/time_client_server/time_server_ipv6.c
@@ -0,0 +1,56 @@
+#include <stdio.h> /* fprintf */
+#include <time.h> /* time */
+#include <string.h> /* memset */
+#include <errno.h>
+#include <unistd.h> /* close */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+#define MAXLINE 32
+
+int main(int argc, char *argv[])
+{
+	int listen_fd, conn_fd;
+	struct sockaddr_in6 serv_addr;
+	char buff[MAXLINE];
+	time_t ticks;
+
+	listen_fd = socket(AF_INET6, SOCK_STREAM, 0);
+	if (listen_fd < 0) {
+		fprintf(stderr, "socket failed:%s\n", strerror(errno));
+		return -1;
+	}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin6_family = AF_INET6;
+	serv_addr.sin6_port = htons(13);
+	serv_addr.sin6_addr = in6addr_any;
+
+	if (bind(listen_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		fprintf(stderr, "bind failed:%s\n", strerror(errno));
+		return -1;
+	}
+
+	if (listen(listen_fd, -1) < 0) {
+		fprintf(stderr, "listen failed:%s\n", strerror(errno));
+		return -1;
+	}
+
+	for (;;) {
+		conn_fd = accept(listen_fd, (struct sockaddr *)NULL, NULL);
+		if (conn_fd < 0) {
+			fprintf(stderr, "accept failed:%s\n", strerror(errno));				return -1;
+		}
+		
+		ticks = time(NULL);
+		snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+		if (write(conn_fd, buff, strlen(buff)) < 0) {
+			fprintf(stderr, "write failed:%s\n", strerror(errno));
+			return -1;
+		}
+
+		close(conn_fd);
+	}
+}
